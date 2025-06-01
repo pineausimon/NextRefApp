d22216b1e3e3c76b02db6c0cb4f0ecfc62d416cd
pineausimon <pineau.simon@lilo.org>
2025-06-01 17:25:13 +0200

    fix lint config

--- nextref-app/eslint.config.js
+++ nextref-app/eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,9 +17,16 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: importPlugin,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {},
+      },
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...importPlugin.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
